a86378da8143c82564460aa576a878b26b97fea8
Shandeep Raula <147018064+Shandeep-Raula@users.noreply.github.com>
2024-11-10 12:58:41 +0530

    Add files via upload

---
+++ sql_query.sql
@@ -0,0 +1,72 @@
+-- create table
+DROP TABLE IF EXISTS spotify;
+CREATE TABLE spotify (
+    artist VARCHAR(255),
+    track VARCHAR(255),
+    album VARCHAR(255),
+    album_type VARCHAR(50),
+    danceability FLOAT,
+    energy FLOAT,
+    loudness FLOAT,
+    speechiness FLOAT,
+    acousticness FLOAT,
+    instrumentalness FLOAT,
+    liveness FLOAT,
+    valence FLOAT,
+    tempo FLOAT,
+    duration_min FLOAT,
+    title VARCHAR(255),
+    channel VARCHAR(255),
+    views FLOAT,
+    likes BIGINT,
+    comments BIGINT,
+    licensed BOOLEAN,
+    official_video BOOLEAN,
+    stream BIGINT,
+    energy_liveness FLOAT,
+    most_played_on VARCHAR(50)
+);
+
+select * from spotify;
+
+
+
+select count(*) from spotify
+
+select count(distinct artist) from spotify
+
+select distinct album_type from spotify
+
+select max(duration_min) from spotify
+
+select min(duration_min) from spotify
+
+select * from spotify
+where duration_min = 0
+
+delete from spotify
+where duration_min = 0
+
+select distinct channel from spotify
+
+select distinct most_played_on from spotify
+
+
+-- Easy 
+
+
+select * from spotify
+where stream > 1000000000;
+
+
+select distinct album 
+from spotify 
+order by 1
+
+
+select sum(comments) from spotify
+where licensed = 'true'
+
+
+
+
